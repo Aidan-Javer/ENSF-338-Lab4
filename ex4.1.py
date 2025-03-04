dfb4072cd4ac1db22a4c57b49a37755abee8cf7c
Tiffany P <tiffany__pham@outlook.com>
2025-03-03 18:02:22 -0700

    Added to 4.1 q2

--- ex4.1.py
+++ ex4.1.py
@@ -10,7 +10,7 @@ The best case complexity would be O(n) and this would be the case where every el
 The average and worst case complexities are O(n^2) since for every element that is greater than 5, the array is iterated over again. On average, the array will be iterated over at least twice.
 
 Question 2:
-The best, average, and worst case complexities are not the same. In the modified version, the complexity of all three cases is O(n^2) since the the array is iterated over twice, regardless of the value of the element. The element is multiplied by two if the element itself is larger than 5.
+The best, average, and worst case complexities are not the same. In the modified version, the complexity of all three cases is O(n^2) since the the array is iterated over twice for each element in the array, regardless of the value of the element. The element is multiplied by two if the element itself is larger than 5.
 """
 
 def processdata_modified(li):
