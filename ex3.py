60604ccc012ca9111ae7d52db1a15523baa1842f
Aidan-Javer <aidan.javer@ucalgary.ca>
2025-03-04 13:14:47 -0700

    Final draft ex3

--- ex3.py
+++ ex3.py
@@ -2,58 +2,67 @@ import sys
 from matplotlib import pyplot as plt
 import timeit
 
-## Part 1 ##
+# Part 1: Growth Strategy Explanation (Question 1)
 """
-Rresizing in the C programs works by allocating new memory when the max capacity has been reached: 
-"new_allocated = ((size_t)newsize + (newsize >> 3) + 6) & ~(size_t)3;". Note that it oversizes the 
-new memory location to add a buffer. Then, once new memory has been allocated it copies the items 
-"self->ob_item = items;" in the old list into the newly allocated one.
+Python's list growth strategy uses an over-allocation formula from lists.c:
+new_allocated = ((size_t)newsize + (newsize >> 3) + 6) & ~(size_t)3; The Growth Factor is 
+approximately 12.5%.
 """
-## Part 2,3,4 ##
 
 s = 0
 
-def test_resize():
-    list = list(range(s))
-    list = list[:]
-    list.append(1)  
-    return list
+# Part 2 ##
 
-
-def test_no_resize():
-    list = list(range(s - 1))
-    list = list[:]  
-    list.append(1)  
-    return list
-
-list = []
-if __name__ in "__main__":
-    prev_size = sys.getsizeof(list)
-    for i in range(64):
-        list.append(i)
-        current_size = sys.getsizeof(list)
+def track_capacity_changes(max_elements):
+    lst = []
+    prev_size = sys.getsizeof(lst)
+    last_trigger_size = 0
+    for i in range(max_elements):
+        lst.append(i)
+        current_size = sys.getsizeof(lst)
         if current_size != prev_size:
-            print(f"After appending {i+1} element(s): size in bytes changed from {prev_size} to {current_size}")
+            print(f"Capacity changed at len={len(lst)} (new sizeof={current_size})")
+            last_trigger_size = len(lst)
             prev_size = current_size
-    s = i + 1
+    return last_trigger_size - 1  
 
-    resize_times = timeit.repeat(lambda: test_resize(), repeat=1000, number=1)
-    no_resize_times = timeit.repeat(lambda: test_no_resize(), repeat=1000, number=1)
+## Part 3 & 4 ##
+def test_resize(lst):
+    lst.append(1)
+    lst.append(1)
 
-    print("Average time for append with resize (S -> S+1):", sum(resize_times) / len(resize_times))
-    print("Average time for append without resize (S-1 -> S):", sum(no_resize_times) / len(no_resize_times))
+def test_no_resize(lst):
+    lst.append(1)     
 
-## Part 5 ## 
+## Part 2 ##
 
-plt.hist(resize_times, bins=30, alpha=0.5, label='Resize (S -> S+1)')
-plt.hist(no_resize_times, bins=30, alpha=0.5, label='No Resize (S-1 -> S)')
-plt.xlabel("Time (seconds)")
-plt.ylabel("Frequency")
-plt.title("Append Operation Time Distribution")
-plt.legend()
-plt.show()
+if __name__ == "__main__":
+    s = track_capacity_changes(64) 
+    print(f"Final S where resize occurs: {s}")
+
+## Parts 3 & 4 ##
+    lst = []
+    for i in range (s - 1):
+        lst.append(i)
+    resize_times = timeit.repeat(lambda: test_resize(lst.copy()), repeat=1000, number=1)
+    no_resize_times = timeit.repeat(lambda: test_no_resize(lst.copy()), repeat=1000, number=1)
+
+    print(f"Avg resize time (S->S+1): {sum(resize_times)/len(resize_times):.8f} sec")
+    print(f"Avg no-resize time (S-1->S): {sum(no_resize_times)/len(no_resize_times):.8f} sec")
+
+## Part 5 ##
+
+    plt.hist(resize_times, bins=30, alpha=0.5, label='Resize (S -> S+1)')
+    plt.hist(no_resize_times, bins=30, alpha=0.5, label='No Resize (S-1 -> S)')
+    plt.xlabel("Time (seconds)")
+    plt.ylabel("Frequency")
+    plt.title("Append Operation Time Distribution")
+    plt.legend()
+    plt.show()
+
 
 """
-Going from a list size of S -> S + 1 takes significantly longer than S - 1 -> S because in the first operation involves the deallocation and reallocation
-new memory, whereas the second operation requires adding/appending the new element without have to resize the list.
+Going from a list size of S -> S + 1 should take longer than S - 1 -> S because the first operation involves the deallocation and reallocation
+of new memory, whereas the second operation requires adding/appending the new element without having to resize the list. However,
+the pyton compiler is extremely quick at reallocating memory that the time is nearly untraceable.
 """
