c503ad5766c77bd89c86c3abe382bdf115f47630
Aidan-Javer <aidan.javer@ucalgary.ca>
2025-03-04 17:59:06 -0700

    Final Draft ex3

--- ex3.py
+++ ex3.py
@@ -9,8 +9,6 @@ new_allocated = ((size_t)newsize + (newsize >> 3) + 6) & ~(size_t)3; The Growth 
 approximately 12.5%.
 """
 
-s = 0
-
 # Part 2 ##
 
 def track_capacity_changes(max_elements):
@@ -27,14 +25,6 @@ def track_capacity_changes(max_elements):
     return last_trigger_size 
 
 ## Part 3 & 4 ##
-def test_resize(lst):
-    lst.append(1)
-    lst.append(1)
-
-def test_no_resize(lst):
-    lst.append(1)     
-
-## Part 2 ##
 
 if __name__ == "__main__":
     s = track_capacity_changes(64) 
@@ -55,38 +45,20 @@ if __name__ == "__main__":
 
     print(f"Avg resize time (S->S+1): {sum(resize_avg_tm)/len(resize_avg_tm):.8f} sec")
     print(f"Avg no-resize time (S-1->S): {sum(no_resize_avg_tm)/len(no_resize_avg_tm):.8f} sec")
-    '''
-## Parts 3 & 4 ##
-    lst = []
-    for i in range (s - 1):
-        lst.append(i)
-    resize_times = timeit.repeat(lambda: test_resize(lst.copy()), repeat=1000, number=1)
-    no_resize_times = timeit.repeat(lambda: test_no_resize(lst.copy()), repeat=1000, number=1)
 
-    print(f"Avg resize time (S->S+1): {sum(resize_times)/len(resize_times):.8f} sec")
-    print(f"Avg no-resize time (S-1->S): {sum(no_resize_times)/len(no_resize_times):.8f} sec")
-    '''
-    plt.hist(resize_avg_tm, bins=30, alpha=0.5, label='Resize (S -> S+1)')
-    plt.hist(no_resize_avg_tm, bins=30, alpha=0.5, label='No Resize (S-1 -> S)')
+
+## Part 5 ##
+
+    plt.hist(resize_avg_tm, bins="auto", edgecolor="black", alpha=0.5, label='Resize (S -> S+1)')
+    plt.hist(no_resize_avg_tm, bins="auto", alpha=0.5, edgecolor="black", label='No Resize (S-1 -> S)')
     plt.xlim(0, 0.00001)
     plt.xlabel("Time (seconds)")
     plt.ylabel("Frequency")
     plt.title("Append Operation Time Distribution")
     plt.legend()
     plt.show()
-## Part 5 ##
 """
-    plt.hist(resize_times, bins=30, alpha=0.5, label='Resize (S -> S+1)')
-    plt.hist(no_resize_times, bins=30, alpha=0.5, label='No Resize (S-1 -> S)')
-    plt.xlabel("Time (seconds)")
-    plt.ylabel("Frequency")
-    plt.title("Append Operation Time Distribution")
-    plt.legend()
-    plt.show()
-"""
-
-"""
-Going from a list size of S -> S + 1 should take longer than S - 1 -> S because the first operation involves the deallocation and reallocation
-of new memory, whereas the second operation requires adding/appending the new element without having to resize the list. However,
-the pyton compiler is extremely quick at reallocating memory that the time is nearly untraceable.
+Going from a list size of S -> S + 1 takes longer than S - 1 -> S because the first operation involves the deallocation and reallocation
+of new memory, whereas the second operation requires adding/appending the new element without having to resize the list. Thus, the first 
+is a O(n) operation whereas, the second is an O(1)
 """
